4e117cd67d54fc284966589dea940af7adccd1ed
Mou Lin <moulin1024@gmail.com>
2024-06-10 19:41:35 +0300

    Add neo-tree

--- init.lua
+++ init.lua
@@ -19,7 +19,19 @@ require("lazy").setup({
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
-   -- LSP Configuration & Plugins
+
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+},
+
+  -- LSP Configuration & Plugins
   {
     'neovim/nvim-lspconfig',
     config = function()
